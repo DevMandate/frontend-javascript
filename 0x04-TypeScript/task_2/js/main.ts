c01493e15f4f158da09b6776ff6bfca1367c160e
DevMandate <wafulacaleb233@gmail.com>
2025-07-10 23:08:18 +0300

    String literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -62,12 +62,28 @@ function executeWork(employee: Director | Teacher): string {
   }
 }
 
+
+// Define the literal type
+type Subjects = 'Math' | 'History';
+
+// Define the function
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else {
+    return 'Teaching History';
+  }
+}
+
 // Testing
 console.log(createEmployee(200));      
 console.log(createEmployee(1000));     
 console.log(createEmployee('$500'));   
 console.log(executeWork(createEmployee(200)));   
 console.log(executeWork(createEmployee(1000)));  
+console.log(teachClass('Math'));     
+console.log(teachClass('History'));  
+
 
 
 
